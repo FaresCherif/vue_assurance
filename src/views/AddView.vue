9c2371a59ba1317ab7303558eadeddd18479221b
Fares <fares.cherif@etu.unilim.fr>
2026-05-04 23:35:58 +0200

    Add homeredirection addView

--- src/views/AddView.vue
+++ src/views/AddView.vue
@@ -1,7 +1,19 @@
 <script setup>
+import { useRouter } from 'vue-router'
+
+import HomeRedirection from '@/components/HomeRedirection.vue';
+
+const router = useRouter();
+
+
+function goHome(){
+    router.push(`/`);
+}
 </script>
 
 
 <template>
+    <HomeRedirection @go-home="goHome"/>
     <h1>Nouveau contrat</h1>
+    
 </template>
