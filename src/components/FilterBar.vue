ae984989c8a3fba3d95f0a903900106e85b346f8
Fares <fares.cherif@etu.unilim.fr>
2026-05-04 20:22:35 +0200

    update filter set select

--- src/components/FilterBar.vue
+++ src/components/FilterBar.vue
@@ -4,13 +4,17 @@ defineEmits(['filterChange'])
 
 <template>
     <div class="filters">
-        <button
-            v-for="statut in ['Tous','Actif','Résilié']"
-            :key="statut"
-            class="btn-filter"
-            @click="$emit('filterChange',statut)"
-        >
+
+        <select @change="$emit('filterChange',$event.target.value)">
+            <option
+                v-for="statut in ['Tous','Actif','Résilié']"
+                :key="statut"
+                class="btn-filter"
+                >
             {{ statut }}
-        </button>
+            </option>
+        </select>
+
+
     </div>
 </template>
